70f2f1630e393e25831d66e9d479048f6d382810
sebastien.pannatier <sebastien.pnntr@eduge.ch>
2023-06-22 11:58:51 +0200

    ✨ feat(domain, actions): Adding actions
    Adding default actions code

--- chatbot/actions/actions.py
+++ chatbot/actions/actions.py
@@ -1,27 +1,56 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class Action_Query_entity(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_query_entity"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="action_query_entity")
+
+        return []
+
+class Action_Query_All_Attribute_Of_Entity(Action):
+
+    def name(self) -> Text:
+        return "action_query_all_attribute_of_entity"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="action_query_all_attribute_of_entity")
+
+        return []
+    
+class Action_Query_Entity_By_Attribute(Action):
+
+    def name(self) -> Text:
+        return "action_query_entity_by_attribute"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="action_query_entity_by_attribute")
+
+        return []
+    
+class Action_Query_Attribute(Action):
+
+    def name(self) -> Text:
+        return "action_query_attribute"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="action_query_attribute")
+
+        return []
